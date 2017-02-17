94a56c3e975123500feecde766963aadde054904
solejay <segun.solaja@gmail.com>
2017-02-17 11:01:40 +0100

    Initial Commit

---
+++ OpenHeavens2017/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenHeavens2017
+//
+//  Created by Segun Solaja on 2/17/17.
+//  Copyright © 2017 Segun Solaja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
